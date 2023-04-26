96fec6d583c4adfbeeee1898998e058d0a66eb04
yossyadirta <yossyadirta@gmail.com>
2023-04-26 20:14:38 +0700

    Initial Structure Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelListContainer, ChannelContainer } from "./components";
 import "./App.css";
+
+const apiKey = process.env.REACT_APP_STREAM_CHAT_API_KEY;
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 }
